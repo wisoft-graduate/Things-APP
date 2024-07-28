8a0b3737f8e23252f6a0da2d1223b80b96ea0263
delaying <pgy5638@naver.com>
2024-07-28 21:50:42 +0900

    타유저 댓글 신고 ui 추가

--- src/screens/Detail/components/CommentComp.tsx
+++ src/screens/Detail/components/CommentComp.tsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react'
-import { Text, TouchableOpacity, View } from 'react-native'
+import { Alert, Text, TouchableOpacity, View } from 'react-native'
 import Icons from 'react-native-vector-icons/Entypo'
 import IonIcons from 'react-native-vector-icons/Ionicons'
 
 import * as ThingsAPI from '../../../api/index'
 import getElapsedHour from '../../../@common/utils/getElapsedHour'
+import { userInfoStore } from '../../../zustand/User'
 
 function CommentComp({ item, childComment, getComments, setReCommentId }) {
   const [isOpenToggle, setIsOpenToggle] = useState<boolean>(false)
+  const { data: userData } = userInfoStore()
 
   const time = new Date(childComment?.createdTime ?? item?.item?.createdTime)
 
@@ -19,6 +21,27 @@ function CommentComp({ item, childComment, getComments, setReCommentId }) {
   }
 
   function Toggle() {
+    const userId = childComment?.userId ?? item?.item?.userId
+    if (userData.id !== userId) {
+      return (
+        <TouchableOpacity
+          onPress={() => {
+            Alert.alert('댓글이 신고처리되었습니다.')
+          }}
+          style={{
+            backgroundColor: 'white',
+            height: 30,
+            paddingHorizontal: 8,
+            paddingVertical: 4,
+            borderRadius: 12,
+            borderWidth: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Text style={{ fontSize: 14, color: 'black', fontWeight: '400' }}>신고</Text>
+        </TouchableOpacity>
+      )
+    }
     return (
       <TouchableOpacity
         onPress={() => deleteComment()}
